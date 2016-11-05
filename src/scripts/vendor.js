e4ce7b7101fcbc5a5a53ab56e9845d8680cc69b8
Bruno Imbrizi <bruno.imbrizi@unit9.com>
2016-11-05 15:40:55 +0000

    moved vendors folder

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -2,4 +2,4 @@ global.ControlKit = require('controlkit');
 global.Sketch = require('sketch-js');
 global.TweenMax = require('gsap');
 global.THREE = require('three');
-require('./../vendor/TrackballControls.js');
+require('./../vendors/TrackballControls.js');
