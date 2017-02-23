2c4f5300e778b028c9d0c3be318e69dbfe40444c
Bruno Imbrizi <brunoimbrizi@Brunos-iMac.local>
2017-02-23 17:58:45 +0000

    added prod (uglify)

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,54 @@
 'use strict';
 
-var gulp        = require('gulp');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream')
+
+var browserify = require('browserify');
+var watchify = require('watchify');
+
 var browserSync = require('browser-sync').create();
-var browserify  = require('browserify');
-var source      = require('vinyl-source-stream')
-var watchify 	= require('watchify');
-var buffer 		= require('vinyl-buffer');
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
 
+var uglify = require('gulp-uglify');
 
-var bundler = watchify(browserify('./src/scripts/main.js', watchify.args));
-    gulp.task('browserify', bundle);
+const isProd = process.env.NODE_ENV === 'production';
 
-bundler.on('update', bundle);     
+// JS
+var bundler = isProd ?
+	browserify('./src/scripts/main.js', watchify.args) :
+	watchify(browserify('./src/scripts/main.js', watchify.args));
+
+bundler.on('update', bundle);
 
 function logError(msg) {
 	console.log( msg.toString() );
 }
 
 function bundle() {
-    var b = bundler.bundle()
-	.on('error', logError)
-	.pipe(source('main.js'))
-	.pipe(buffer())
-	.pipe(gulp.dest('./dist/scripts/'))
-	.pipe(reload({stream: true, once: true}));
-
-    return b;
+	var b = bundler.bundle()
+		.on('error', logError)
+		.pipe(source('main.js'))
+		.pipe(buffer())
+		.pipe(isProd ? uglify() : gutil.noop())
+		.pipe(gulp.dest('./dist/scripts/'))
+		.pipe(reload({stream: true, once: true}));
+	return b;
 }
 
+gulp.task('browserify', bundle);
+
+// CSS
 gulp.task('watch', function() {
 	gulp.watch('./src/scss/*.scss', ['sass']);
 });
 
-
 gulp.task('sass', function() {
-	return sass('./src/scss/main.scss') 
+	return sass('./src/scss/main.scss')
 	.on('error', function (err) {
-	  console.error('Error!', err.message);
+		console.error('Error!', err.message);
 	})
 	.pipe(prefix({
 			browsers: ['last 2 versions'],
@@ -47,13 +58,22 @@ gulp.task('sass', function() {
 	.pipe(reload({stream:true}));
 });
 
-gulp.task('browser-sync', function() {
+// Server
+gulp.task('serve', function() {
 	browserSync.init({
-	    server: {
-	      baseDir: './dist/'
-	    },
-	    reloadDebounce: 500
-	  });
+		server: {
+			baseDir: './dist/'
+		},
+		reloadDebounce: 500
+	});
 });
 
-gulp.task('default', ['browserify', 'browser-sync', 'watch']);
+// Default
+let tasks = [];
+if (isProd) {
+	tasks = ['browserify'];
+} else {
+	tasks = ['browserify', 'serve', 'watch'];
+}
+
+gulp.task('default', tasks);
