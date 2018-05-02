61204dc1dc4a425909fbe21f59e2344b1803fe84
brunoimbrizi <bruno.imbrizi@gmail.com>
2018-05-02 23:13:19 +0100

    Add AsyncPreloader

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,8 +1,24 @@
+import AsyncPreloader from 'async-preloader';
+
 import AppView from './view/AppView';
 
 export default class App {
 
 	constructor() {
+		this.initLoader();
+	}
+
+	initLoader() {
+		AsyncPreloader.loadManifest('data/manifest.json')
+		.then(items => {
+			this.initView();
+		})
+		.catch(err => {
+			console.log('AsyncPreloader error', err);
+		});
+	}
+
+	initView() {
 		this.view = new AppView();
 	}
 }
